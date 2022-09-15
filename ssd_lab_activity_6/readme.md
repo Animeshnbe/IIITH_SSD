8f80496046cbd89f90163dc086ea6ec38214d933
Animeshnbe <das1ani812@gmail.com>
2022-09-15 18:13:48 +0530

    lab 6

---
+++ ssd_lab_activity_6/readme.md
@@ -0,0 +1,7 @@
+SSD LAB Assignment 6
+
+Open index.html in browser to see the output
+
+Form fields are validated as requirement and shown as alert upon submission.
+
+Dark Mode is added!
---
+++ ssd_lab_activity_6/script.js
@@ -0,0 +1,73 @@
+document.addEventListener ("keydown", function (zEvent) {
+    if (zEvent.ctrlKey  &&  zEvent.key === "m") {  
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+    }
+} );
+
+    
+function getmem(memb,k){
+    return memb+k.innerText;
+}
+
+function val(){
+    var mem = document.querySelectorAll(".sol p");
+    var memb = "";
+    for (let m of mem){
+        memb+=(m.textContent+", ");
+    }
+    // alert(memb.slice(0,-2));
+    // return false;
+
+
+    alert("Name: "+document.myForm.mname.value+
+            "\nEmail: "+document.myForm.gemail.value+
+            "\nLead: "+document.myForm.lead.value+
+            "\nMembers: "+memb.slice(0,-2));
+    return true;
+}
+function unameval(event){
+    // event.preventDefault();
+    
+    let uname=event.target.value;
+    let re=/(?=.*\d)(?=.*[A-Z])/;
+    // alert(document.getElementByClassName("uerror").value);
+    if (uname==""){
+        document.getElementById("uerror").innerHTML = "" ;
+        return false;
+    }
+    else if (re.test(uname)){
+        document.getElementById("uerror").innerHTML = "Valid Username" ;
+        return true;
+    }
+    else{
+        document.getElementById("uerror").innerHTML = "Username must have Uppercase and number!" ;
+        return false;
+    }
+}
+function dsHandler(e){
+    e.dataTransfer.setData("elem",e.target.id);
+}
+
+function doHandler(e){
+    e.preventDefault();
+}
+
+function dpHandler(e){
+    e.preventDefault();
+    var elId = e.dataTransfer.getData("elem");
+    e.target.appendChild(document.getElementById(elId));
+}
+
+function confirmP() {
+    if (document.querySelector("[name='pwd']").value == "" ) {
+        document.getElementById('message').innerHTML = '';
+    }
+    else if (document.querySelector("[name='pwd']").value == document.querySelector("[name='cpwd']").value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'matching';
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'not matching';
+    }
+}
